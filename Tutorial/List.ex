db4c783cb7f2bf9dff864f9b0325b22d3b296ecd
Tae Geun, Kim <edeftg@gmail.com>
2017-12-08 18:38:13 +0900

    Elixir Style Coding
    Fix List.ex

--- Tutorial/List.ex
+++ Tutorial/List.ex
@@ -1,13 +1,7 @@
 defmodule FPlist do
-  def main() do
-    x = [1, 2, 3, 4, 5]
-    plus(x) |> IO.inspect
-  end
-
   def plus([]), do: []
-  def plus([head | tail]) do
-    [head+1 | plus(tail)]
-  end
+  def plus([head | tail]), do: [head+1 | plus(tail)]
 end
 
-FPlist.main()
+x = [1, 2, 3, 4, 5]
+FPlist.plus(x) |> IO.inspect
